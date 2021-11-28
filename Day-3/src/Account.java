f4353541a2108fd073a51402663a7147bfe44e51
Mydhili-murali <mydhilimurali2@gmail.com>
2021-11-28 11:58:26 +0530

    java basic programs

--- Day-3/src/Account.java
+++ Day-3/src/Account.java
@@ -5,14 +5,14 @@ class BankAccount{
     String name;
     String accno;
     int total=1000;
-    public BankAccount(String name, String accno)
+    BankAccount(String name, String accno)
     {
         this.name=name;
         this.accno=accno;
     }
     public void display()
     {
-        System.out.println("Name:"+this.name +" \nAccount number: "+ this.accno);
+        System.out.println("Name:"+ this.name +" \nAccount number: "+ this.accno);
     }
 
 
@@ -24,11 +24,12 @@ class BankAccount{
     }
 
     public void withdrawal(int amt) {
-        if(total>amt )
+        if(total>amt)
         {
-            System.out.println(name + " withdrawn " + amt + " from account number " + accno);
-            total = total - amt;
-            System.out.println("Total amount after withdrawal is " + total);
+
+                System.out.println(name + " withdrawn " + amt + " from account number " + accno);
+                total = total - amt;
+                System.out.println("Total amount after withdrawal is " + total);
 
 
 
@@ -49,22 +50,35 @@ public class Account {
         String name = scanner.nextLine();
         System.out.println("Enter the account number: ");
         String account = scanner.nextLine();
-        BankAccount ob1 = new BankAccount(name, account);
-        System.out.println("Enter a choice from( 1- 3 )......1.Display details....2.Deposit......3.Withdrawal.........");
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1 -> ob1.display();
-            case 2 -> {
-                System.out.println("Enter the amount to be deposit:");
-                int amount = scanner.nextInt();
-                ob1.deposit(amount);
+        BankAccount ob1=new BankAccount(name,account);
+        while(true) {
+            System.out.println("Enter a choice from( 1- 3 )......1.Display details....2.Deposit......3.Withdrawal.........");
+            int choice = scanner.nextInt();
+            if (choice >= 4) {
+                break;
             }
-            case 3 -> {
-                System.out.println("Enter the amount to be withdraw: ");
-                int amt = scanner.nextInt();
-                ob1.withdrawal(amt);
+
+            switch (choice) {
+                case 1:
+
+                    ob1.display();
+                    break;
+                case 2:
+                    System.out.println("Enter the amount to be deposit:");
+                    int amount = scanner.nextInt();
+                    ob1.deposit(amount);
+                    break;
+                case 3:
+                    System.out.println("Enter the amount to be withdraw: ");
+                    int amt = scanner.nextInt();
+                    ob1.withdrawal(amt);
+                    break;
+
+                default:
+                    System.out.println("Enter valid choice....");
+                    break;
             }
-            default -> System.out.println("Enter valid choice....");
+
         }
 
     }
