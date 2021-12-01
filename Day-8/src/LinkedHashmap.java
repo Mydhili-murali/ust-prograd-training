b5ad84dfb6991efe59ad65c8444d59d0dd28cc59
Mydhili-murali <mydhilimurali2@gmail.com>
2021-12-01 17:24:41 +0530

    Interactive dictionary program_HashMap

---
+++ Day-8/src/LinkedHashmap.java
@@ -0,0 +1,7 @@
+import java.util.LinkedHashMap;
+
+public class LinkedHashmap {
+    public static void main(String[] args){
+        LinkedHashMap<Integer><String> lh=new LinkedHashMap();
+    }
+}
