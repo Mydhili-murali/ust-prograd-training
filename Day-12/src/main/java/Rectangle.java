5e1f95985db7576b8f1cf005bf9701e977c966b6
Mydhili-murali <mydhilimurali2@gmail.com>
2021-12-08 22:16:25 +0530

    Area and Perimeter of Rectangle-TDD

--- Day-12/src/main/java/Rectangle.java
+++ Day-12/src/main/java/Rectangle.java
@@ -1,16 +1,19 @@
-public class Rectangle {
-    public double area(double l, double b){
-        if(l<=0||b<=0||l==b){
-            throw new IllegalArgumentException("Cannot find area of rectangle");
-        }
-        return l*b;
 
-    }
 
-    public double perimeter(double l, double b) {
-        if(l<=0||b<=0||l==b){
-            throw new IllegalArgumentException("Cannot find perimeter of rectangle");
+    public class Rectangle {
+        public double area(double l, double b) {
+            if (l <= 0 || b <= 0 || l == b) {
+                throw new IllegalArgumentException("Cannot find area of rectangle");
+            }
+            return l * b;
+
         }
-        return 2*(l+b);
+
+        public double perimeter(double l, double b) {
+            if (l <= 0 || b <= 0 || l == b) {
+                throw new IllegalArgumentException("Cannot find perimeter of rectangle");
+            }
+            return 2 * (l + b);
+
+        }
     }
-}
