030274389b379bb5e6fbb3fb8e8fb6de4ebe7d63
Mydhili-murali <mydhilimurali2@gmail.com>
2021-12-12 18:46:32 +0530

    Architect problem for length converter and weight converter-TDD

---
+++ Day-15/src/main/java/LengthConverter.java
@@ -0,0 +1,17 @@
+//import java.io.IOException;
+
+class LengthConverter {
+
+    public double centimeterToCentimeter(double lengthInCentimeter){return lengthInCentimeter;}
+    public double centimeterToMeter(double lengthInCentimeter){
+        return lengthInCentimeter/100;
+    }
+    public double meterToCentimeter(double lengthInMeter){return lengthInMeter*100;}
+    public double centimeterToKilometer(double lengthInCentimeter){
+        return lengthInCentimeter/100000;
+    }
+    public double kilometerToCentimeter(double lengthInKilometer){return lengthInKilometer*100000;}
+
+
+
+}
---
+++ Day-15/src/main/java/WeightConverter.java
@@ -0,0 +1,12 @@
+public class WeightConverter {
+    public double gramToGram(double weightInGram) {
+        return weightInGram;
+    }
+
+    public double kilogramToGram(double weightInKilogram) {
+        return weightInKilogram*1000;
+    }
+    public double gramToKiloGram(double weightInGram) {
+        return weightInGram/1000;
+    }
+}
