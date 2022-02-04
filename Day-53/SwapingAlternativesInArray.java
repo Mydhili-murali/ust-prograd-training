2e3ae111f5682b4ac1a9385c4cfdfe835961abd5
Mydhili-murali <mydhilimurali2@gmail.com>
2022-02-04 18:29:03 +0530

    competitive code practices

--- Day-53/SwapingAlternativesInArray.java
+++ Day-53/SwapingAlternativesInArray.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 class SwapingAlternativesInarray {
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size:");
-        int size=sc.nextInt();
+        int size = sc.nextInt();
 
-        int array[]=new int[size];
+        int array[] = new int[size];
         System.out.println("Enter the elements:");
-        for(int i=0;i<array.length;i++){
-            array[i]=sc.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
         }
-        for(int i=0;i<array.length-1;i+=2){
-            int temp=array[i];
-            array[i]=array[i+1];
-            array[i+1]=temp;
+        for (int i = 0; i < array.length - 1; i += 2) {
+            int temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
         }
-        for(int i=0;i<array.length;i++){
+        for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
 
